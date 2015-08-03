9982e6959984b1379913f7a27fdce9ba96908ca3
Tommy Svensson <tommy@natusoft.se>
2015-08-03 20:43:03 +0200

    mdd-23: Fixed header formatting in editor, now starting at the char directly after the last #.

--- Editor/src/main/groovy/se/natusoft/doc/markdowndoc/editor/MarkdownStyler.groovy
+++ Editor/src/main/groovy/se/natusoft/doc/markdowndoc/editor/MarkdownStyler.groovy
@@ -3,31 +3,31 @@
  * PROJECT
  *     Name
  *         MarkdownDocEditor
- *     
+ *
  *     Code Version
  *         1.4
- *     
+ *
  *     Description
  *         An editor that supports editing markdown with formatting preview.
- *         
+ *
  * COPYRIGHTS
  *     Copyright (C) 2012 by Natusoft AB All rights reserved.
- *     
+ *
  * LICENSE
  *     Apache 2.0 (Open Source)
- *     
+ *
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
  *     You may obtain a copy of the License at
- *     
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  *     Unless required by applicable law or agreed to in writing, software
  *     distributed under the License is distributed on an "AS IS" BASIS,
  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
- *     
+ *
  * AUTHORS
  *     Tommy Svensson (tommy@natusoft.se)
  *         Changes:
@@ -418,7 +418,7 @@ class MarkdownStyler implements Configurable, JTextComponentStyler {
                                 ++cnt
                                 ++pos
                             }
-                            int epos = getPosOfNext(text, pos, (char)'\n')
+                            int epos = getPosOfNext(text, pos, '\n' as char)
                             Style header = null
 
                             // Only style if it starts at the beginning of a "paragraph".
@@ -427,26 +427,26 @@ class MarkdownStyler implements Configurable, JTextComponentStyler {
                                 switch(cnt) {
                                     case 1:
                                         header = this.h1Style
-                                        hsize = 2
+                                        hsize = 1
                                         break
                                     case 2:
                                         header = this.h2Style
-                                        hsize = 3
+                                        hsize = 2
                                         break
                                     case 3:
                                         header = this.h3Style
-                                        hsize = 4
+                                        hsize = 3
                                         break
                                     case 4:
                                         header = this.h4Style
-                                        hsize = 5
+                                        hsize = 4
                                         break
                                     case 5:
                                         header = this.h5Syle
-                                        hsize = 6
+                                        hsize = 5
                                         break
                                     default:
-                                        hsize = 7
+                                        hsize = 6
                                         header = this.h6Style
                                 }
                             }
