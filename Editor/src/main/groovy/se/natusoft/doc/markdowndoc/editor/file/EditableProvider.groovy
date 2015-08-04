304a3e3aa65bea83ea211bdf84e334a046b48b7b
Tommy Svensson <tommy@natusoft.se>
2015-08-04 11:48:59 +0200

    mdd-24 Auto styling when typing have been lost! Ctrl/Cmd-R still works.

--- Editor/src/main/groovy/se/natusoft/doc/markdowndoc/editor/file/EditableProvider.groovy
+++ Editor/src/main/groovy/se/natusoft/doc/markdowndoc/editor/file/EditableProvider.groovy
@@ -1,38 +1,38 @@
-/* 
- * 
+/*
+ *
  * PROJECT
  *     Name
  *         MarkdownDocEditor
- *     
+ *
  *     Code Version
  *         1.4
- *     
+ *
  *     Description
  *         An editor that supports editing markdown with formatting preview.
- *         
+ *
  * COPYRIGHTS
  *     Copyright (C) 2012 by Natusoft AB All rights reserved.
- *     
+ *
  * LICENSE
  *     Apache 2.0 (Open Source)
- *     
+ *
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
  *     You may obtain a copy of the License at
- *     
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  *     Unless required by applicable law or agreed to in writing, software
  *     distributed under the License is distributed on an "AS IS" BASIS,
  *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
- *     
+ *
  * AUTHORS
  *     tommy ()
  *         Changes:
  *         2015-08-03: Created!
- *         
+ *
  */
 package se.natusoft.doc.markdowndoc.editor.file
 
@@ -40,6 +40,7 @@ import groovy.transform.CompileStatic
 import groovy.transform.ToString
 import groovy.transform.TypeChecked
 import org.jetbrains.annotations.NotNull
+import se.natusoft.doc.markdowndoc.editor.OSTrait
 import se.natusoft.doc.markdowndoc.editor.api.*
 
 import javax.swing.*
@@ -59,7 +60,7 @@ import java.awt.event.MouseMotionListener
 @CompileStatic
 @TypeChecked
 @ToString(includeNames = true)
-class EditableProvider implements Editable {
+class EditableProvider implements Editable, OSTrait {
     //
     // Private Members
     //
@@ -68,13 +69,7 @@ class EditableProvider implements Editable {
 
     /** The actual editor instance for this file. */
     private JTextPane editorPane = new JTextPane() {
-        @Override
-        Dimension getPreferredSize() {
-            final Dimension dim = super.getPreferredSize()
-            dim.setSize(getWidth(), dim.getHeight())
 
-            return dim
-        }
         @Override
         Dimension getMinimumSize() {
             return getPreferredSize()
@@ -123,8 +118,7 @@ class EditableProvider implements Editable {
         String undoKey = "control Z"
         String redoKey = "control Y"
 
-        final String osName = System.getProperty("os.name").toUpperCase()
-        if (osName.contains("MAC")) {
+        if (macOSXOS) {
             undoKey = "meta Z"
             redoKey = "shift meta Z"
         }
@@ -136,6 +130,11 @@ class EditableProvider implements Editable {
                 EditableProvider.this.undoManager.addEdit(evt.edit);
             }
         })
+
+        // This is incredibly important since only this document model have insertString(...)
+        // and remove() overridden to automatically update styling as the model is updated.
+        // Also only this model has the configured styling fonts and sizes.
+        this.editorPane.document = doc
 
         this.editorPane.getActionMap().put("Undo", new AbstractAction("Undo") {
             public void actionPerformed(final ActionEvent evt) {
